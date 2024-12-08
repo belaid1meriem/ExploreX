fb49ffe9f30e53f2e3ed6a4ee8448256085d2982
owlislem <ibchht@gmail.com>
2024-12-08 14:19:13 +0100

    vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import commonjs from "@rollup/plugin-commonjs";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ["swiper"], // Externalize swiper if needed
+      plugins: [
+        commonjs(), // Add CommonJS support
+      ],
+    },
+  },
 });
